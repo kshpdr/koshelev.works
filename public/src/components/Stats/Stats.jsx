5e7d49d8f20746ab9e0ece33e86c4f72bce41e20
kshpdr <truemacintoshchannel@gmail.com>
2023-04-18 13:15:11 +0200

    Add manually books

--- public/src/components/Stats/Stats.jsx
+++ public/src/components/Stats/Stats.jsx
@@ -8,8 +8,11 @@ function Stats({ year, onYearChange }) {
   const [showReviewModal, setShowReviewModal] = createSignal(false);
 
   const books = [
-    // { id: 1, title: "Book 1", dateFinished: "2023-01-01" },
-    // { id: 2, title: "Book 2", dateFinished: "2023-01-15" },
+    { id: 1, title: "1948 - George Orwell", dateFinished: "26.01.2023" },
+    { id: 2, title: "Brave New World - Aldous Huxley", dateFinished: "23.02.2023" },
+    { id: 3, title: "Kafka on the Shore - Haruki Murakami", dateFinished: "21.03.2023" },
+    { id: 4, title: "Unfaithfulness - Marina Trawkova", dateFinished: "26.03.2023" },
+    { id: 5, title: "Great Expectations - Charles Dickens (Dropped)", dateFinished: "17.04.2023" },
   ];
 
   const films = [
@@ -46,14 +49,13 @@ function Stats({ year, onYearChange }) {
         </YearDropdown>
       </h2>
       <h3>Books</h3>
-      {/* <ul>
+      <ul>
         {books.map((book) => (
-          <li key={book.id} onClick={() => handleBookClick(book)}>
-            {book.title} ({book.dateFinished})
+          <li key={book.id}>
+            <b>{book.dateFinished}:</b> {book.title}
           </li>
         ))}
-      </ul> */}
-      <div>Soon... Have to write an API call for that.</div>
+      </ul>
       <h3>Films</h3>
       {/* <ul>
         {films.map((film) => (
@@ -62,7 +64,7 @@ function Stats({ year, onYearChange }) {
           </li>
         ))}
       </ul> */}
-      <div>Same here.</div>
+      <div>Soon... Have to write an API call for that. For now <a href="https://letterboxd.com/koshelev/films/">letterboxd</a></div>
       {showReviewModal() && (
         <Modal
           item={selectedBook() || selectedFilm()}
